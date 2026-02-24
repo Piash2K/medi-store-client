cd9ad024c4edcdc05e45885025a326d73e5d5ff4
Piash Islam <piashislam2k@gmail.com>
2026-02-24 21:34:58 +0600

    parallel routing added for role based access

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -75,7 +75,7 @@ export function Navbar() {
    setUser(null);
   };
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/80">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/80">
       <div className="mx-auto w-full  px-4 sm:px-6 lg:px-8">
         <nav className="hidden h-16 items-center justify-between lg:flex">
           <div className="flex items-center gap-6">
